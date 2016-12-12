e92a8cdcb8928619cc809147b49231640e4d59e1
tofid <andreyklochok@gmail.com>
2016-12-12 17:21:34 +0200

    minor

--- src/views/layouts/_splash.php
+++ src/views/layouts/_splash.php
@@ -1,7 +1,14 @@
 <div class="container">
     <div class="intro-text">
-        <div class="intro-lead-in">Welcome To Our Studio!</div>
-        <div class="intro-heading">It's Nice To Meet You</div>
-        <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+        <div class="intro-lead-in">Control all your services from a single place</div>
+        <div class="intro-heading">NEXT GENERATION CONTROL PANEL</div>
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-2">
+                <a href="#features" class="page-scroll btn btn-xl btn-block">Tell Me More</a>
+            </div>
+            <div class="col-sm-4">
+                <a href="/site/signup" class="page-scroll btn btn-xl btn-block">Try it out</a>
+            </div>
+        </div>
     </div>
 </div>
